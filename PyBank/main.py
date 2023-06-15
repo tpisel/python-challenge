bd16c7a29710dfa926b1b52f7187331ccf9946fe
Tom Pisel <mail@tompisel.com>
2023-06-15 23:30:28 +1000

    Updates to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,31 +2,40 @@ import os
 import pandas as pd
 
 # ingest
-filepath = os.path.join("Resources","budget_data.csv")
-os.path.isfile(filepath)
+wd = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(wd,'Resources','budget_data.csv')
 df = pd.read_csv(filepath)
+header_row = list(df.columns)
 pl = df['Profit/Losses']
+
+# transform
 df['diff'] = pl.diff()
 
 # calculate
 total_months = len(df)
 total_profit_loss = pl.sum()
 average_change = pl.diff().mean()
-greatest_increase_row = df[pl.diff() == pl.diff().max()]
-greatest_decrease_row = df[pl.diff() == pl.diff().min()]
+inc_row = df[pl.diff() == pl.diff().max()]
+dec_row = df[pl.diff() == pl.diff().min()]
 
-# print
+# compose output text
 f = r'{:,.2f}' # number format
-print('Financial Analysis')
-print('----------------------------')
-print('Total Months:', total_months)
-print('Total:', f.format(total_profit_loss))
-print('Average Change:', f.format(average_change))
-print(
-    'Greatest Increase in Profits:',
-    greatest_increase_row['Date'].values[0],
-    f.format(greatest_increase_row['diff'].values[0]))
-print(
-    'Greatest Decrease in Profits:',
-    greatest_decrease_row['Date'].values[0],
-    f.format(greatest_decrease_row['diff'].values[0]))
+output_text = \
+f"""
+Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: {f.format(total_profit_loss)}
+Average Change: {f.format(average_change)}
+Greatest Increase in Profits: {inc_row['Date'].values[0]} ({f.format(inc_row['diff'].values[0])})
+Greatest Decrease in Profits: {dec_row['Date'].values[0]} ({f.format(dec_row['diff'].values[0])})
+"""
+
+# output
+print(output_text)
+output_file = os.path.join(wd,'output.txt')
+
+with open(output_file, 'w') as txt:
+    txt.write(output_text)
+
+print('Saved to', output_file)
